41f79d72fd1e4bedd8a544675487647a93146186
BenSnedeker <BenSnedeker1@gmail.com>
2025-01-06 22:45:38 -0600

    Few changes

--- gowiki/api.go
+++ gowiki/api.go
@@ -1,12 +1,18 @@
 package gowiki
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// -------------
+//    SEARCH
+// -------------
 
 func HandleSearch(query string) {
 	fmt.Printf("Searching for \033[1;34m\"%s\"\033[0m\n", query)
 }
+
+// -------------
+//     READ
+// -------------
 
 func HandleRead(flags Flags, query string) {
 	fmt.Printf("Opening article \033[1;34m\"%s\"\033[0m\n", query)
